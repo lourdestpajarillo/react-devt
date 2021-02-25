18a689104b7233be0deb0915a3d2de573ca964bc
Lourdes Pajarillo <lourdes.tupas@gmail.com>
2021-02-24 23:01:26 -0800

    header, sidebar, main

---
+++ lourdestp-slack-clone-challenge/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Container>
+            <Main>
+                
+            </Main>
+            {/* <UserContainer>
+
+            </UserContainer> */}
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+
+`
---
+++ lourdestp-slack-clone-challenge/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div style={{ background: "blue" }}>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
